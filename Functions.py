06cb1f2ff0baa83dbda44aefa5a70b5de2efe96f
Harry He <harry.r.he@oocl.com>
2018-11-12 23:10:39 +0800

    functions learning

---
+++ Functions.py
@@ -0,0 +1,21 @@
+print(abs(-200))
+print(max(1, 2, 3))
+print(min(1, 2, 3))
+print(int('123'))
+
+
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+
+print(my_abs(-99))
+
+
+def power(x):
+    return x * x
+
+
+print(power(9))
