6d748e2db60d2225daeb65222cf61d54e3fa6661
Harry He <harry.r.he@oocl.com>
2018-11-15 10:32:09 +0800

    TDD

---
+++ TDD/Dict.py
@@ -0,0 +1,12 @@
+class Dict(dict):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError("object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
